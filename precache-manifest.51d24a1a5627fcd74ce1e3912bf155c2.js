ae7cb6d40e30e60103f9cf6cea454ed73ee237f4
Deb Yu <debora.yu@gmail.com>
2020-08-10 16:41:38 -0400

    Updates

---
+++ precache-manifest.51d24a1a5627fcd74ce1e3912bf155c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815b3310bb588d2a32bdb2f22d6917fc",
+    "url": "/DeboraYuProjectFive/index.html"
+  },
+  {
+    "revision": "9c16d9354a9cc2050993",
+    "url": "/DeboraYuProjectFive/static/css/main.93b06191.chunk.css"
+  },
+  {
+    "revision": "db8cc876dcd1c23f2ff8",
+    "url": "/DeboraYuProjectFive/static/js/2.0afa7182.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DeboraYuProjectFive/static/js/2.0afa7182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c16d9354a9cc2050993",
+    "url": "/DeboraYuProjectFive/static/js/main.6e9b9899.chunk.js"
+  },
+  {
+    "revision": "8b5cbaa62de565ce5dc6",
+    "url": "/DeboraYuProjectFive/static/js/runtime-main.168b5fcf.js"
+  }
+]);
